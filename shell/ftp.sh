fa9ea155be190475ccad35b54228728700e23055
chong.jin <chong.jin@tophant.com>
2023-12-01 15:34:37 +0800

    优化ftp脚本

--- shell/ftp.sh
+++ shell/ftp.sh
@@ -2,7 +2,7 @@
   
 # Check if correct number of arguments provided
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <p/put|g/get> <filename>"
+    usage
     exit 1
 fi
 
@@ -10,39 +10,49 @@ fi
 FTP_SERVER="192.168.213.220"
 FTP_USERNAME="admin"
 FTP_PASSWORD="123456"
+METHOD=$1
+X_FILE=$2
+
+# Function usage
+usage() {
+    echo "Usage: $0 <p/put|g/get> <filename>"
+    exit 1
+}
 
 # Function to upload a file
 upload_file() {
+    FILE=$1
     ftp -n -i "$FTP_SERVER" <<END_SCRIPT
 quote USER $FTP_USERNAME
 quote PASS $FTP_PASSWORD
 binary
-put "$1"
+put "$FILE"
 quit
 END_SCRIPT
 }
 
 # Function to download a file
 download_file() {
+    FILE=$1
     ftp -n -i "$FTP_SERVER" <<END_SCRIPT
 quote USER $FTP_USERNAME
 quote PASS $FTP_PASSWORD
 binary
-get "$1"
+get "$FILE"
 quit
 END_SCRIPT
 }
 
 # Main script
-case "$1" in
+case "$METHOD" in
     p|put)
-        upload_file "$2"
+        upload_file "$X_FILE"
         ;;
     g|get)
-        download_file "$2"
+        download_file "$X_FILE"
         ;;
     *)
-        echo "Invalid option. Usage: $0 <p/put|g/get> <filename>"
+        usage
         exit 1
         ;;
 esac
